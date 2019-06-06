84ee50185ebf9581ae3aed941829be10541e4adb
Martha Plantz <maplantz89@gmail.com>
2019-06-06 14:33:20 -0700

    newest version including adding an item to the list

--- index.js
+++ index.js
@@ -15,6 +15,40 @@ $('.shopping-item-toggle').click(event => {
   event.preventDefault();
   console.log(event.currentTarget);
   $(event.currentTarget).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
-
 }
 );
+
+/**
+ * add user text to list item when clicking 'Add item' button Or hitting return 
+ * store user text and add that text to the list when you click button or hit return 
+ */
+
+
+//when you click submit ... add text to list 
+
+$('#js-shopping-list-form').submit(event => {
+  event.preventDefault();
+  //this is the users text
+  const newEntry = $(event.currentTarget).find('#shopping-list-entry');
+  const testEntry = newEntry.val();
+  console.log(newEntry);
+  //adding user text to the list 
+  $('ul').html( `    
+  <li>
+  <span class="shopping-item">${testEntry}</span>
+  <div class="shopping-item-controls">
+    <button class="shopping-item-toggle">
+      <span class="button-label">check</span>
+    </button>
+    <button class="shopping-item-delete">
+      <span class="button-label">delete</span>
+    </button>
+  </div>
+</li>' `
+  );
+
+  //deleting the users text from the box
+  newEntry.val('');
+
+});
+
